49a8f15d4d4ac5c13640bfeb24266547aadd36ac
Romith Mahmood <tmromith@gmail.com>
2024-12-20 20:54:23 +0000

    Input field and column organized
    Added an input field where a user will be the URL of a TikTok account.

--- test.py
+++ test.py
@@ -11,15 +11,10 @@ service = Service("C:\\Users\\tmrom\\OneDrive\\Desktop\\Python\\geckodriver.exe"
 driver = webdriver.Firefox(service=service)
 
 # Navigate to the website
-website = input("Enter the URL of TikTok account: ")
-# website = "https://www.tiktok.com/@uccbangladeshsociety"
+website = "https://www.tiktok.com/@uccbangladeshsociety"
 driver.get(website)
-print("Processing...")
 
 time.sleep(6)
-
-# A variable i.e. the_title, followers_count etc.. were assigned. If I didn't add this, 
-# then the printed values will come out as a string representation of the web element.
 
 # Retrieve the account name.
 title = driver.find_element(By.XPATH, '//*[@id="main-content-others_homepage"]/div/div[1]/div[2]/div[1]/div/h2')
@@ -36,26 +31,10 @@ likes_element = driver.find_element(By.CSS_SELECTOR, 'strong[data-e2e="likes-cou
 likes_count = likes_element.text
 print("Number of likes",likes_count)
 
-time.sleep(3)
-
-views = driver.find_elements(By.CSS_SELECTOR, 'strong[data-e2e="video-views"]')
-
-# Retrieve the number of views for each video
-# Each video is assigned a number.
-
-for x, view in enumerate(views, start=1):
-    print(f"Video {x} Views:", view.text)
-
-# Save the results to a csv file.
-
 with open("Bsoc tiktok.csv","w",newline="",encoding="utf-8") as f:
     write = csv.writer(f)
 
-    write.writerow(["ACCOUNT","FOLLOWERS","TOTAL-LIKES","VIDEO","VIEWS"])
-    write.writerow([the_title,followers_count,likes_count,"Video",x,view.text])
-
-    # write.writerow(["Video","Views"])
-    # write.writerow([view.text])
+    write.writerow(["ACCOUNT","FOLLOWERS","TOTAL-LIKES"])
+    write.writerow([the_title,followers_count,likes_count])
 
 driver.quit()
-
